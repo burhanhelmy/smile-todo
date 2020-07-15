51831c568efad2d03d24123003314ec5c30ea843
Burhan <burhanuddin.j@vettons.com>
2020-07-15 10:08:35 +0800

    wip ui design

---
+++ lib/module/todo-list/screen/todo-list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:smile_todo/module/todo-list/widget/todo-card.dart';
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+          title: Text(
+            "To-Do List",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: false),
+      body: _renderList(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[500],
+        onPressed: null,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  _renderList() {
+    return ListView(
+      padding: EdgeInsets.all(20),
+      children: <Widget>[TodoCard()],
+    );
+  }
+}
---
+++ lib/module/todo-list/widget/todo-card.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class TodoCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 3,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    "Automated Testing Script",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _renderLabelAndValue("Start Date", "21 Oct 2019"),
+                        _renderLabelAndValue("Start Date", "21 Oct 2019"),
+                        _renderLabelAndValue("Start Date", "21 Oct 2019")
+                      ]),
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.grey[300],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Status:",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.grey),
+                      ),
+                      Text(
+                        "Incomplete",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Tick if completed",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Checkbox(value: false, onChanged: null),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _renderLabelAndValue(label, value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+        ),
+        Text(
+          value,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
